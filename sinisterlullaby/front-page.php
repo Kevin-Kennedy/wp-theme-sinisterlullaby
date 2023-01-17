5b4e6bd26e0343bb14c6a445a4a1bc003d97deed
Kevin-Kennedy <kevin.kennedy.tx@gmail.com>
2023-01-17 01:00:08 -0600

    Refactor header/footer

--- sinisterlullaby/front-page.php
+++ sinisterlullaby/front-page.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title></title>
+<?php
+    get_header();
+?>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name=viewport" content="width=device-width, initial-scale=1.0">
-
-    <?php
-        wp_head();
-    ?>
-</head>
-<body>
+<p>Content</p>
 
 <?php
-    wp_footer();
+    get_footer();
 ?>
-</body>
-</html>
